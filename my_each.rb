198c5803ce30d724f539285690c06f233bd0ddcf
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-27 23:05:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr_list)
+  index = 0
+  while index < arr_list.size
+    yield(arr_list[index])
+    index += 1
+  end
+  arr_list
 end
